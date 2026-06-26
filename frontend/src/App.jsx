016ce56dc88277304e1aae4e166a416749192762
Aditya Thakur <adityathakur6176@gmail.com>
2026-06-26 09:17:14 +0545

    /map added but not working

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Timeline from './pages/Timeline.jsx'
 import DayView from './pages/DayView.jsx'
 import NotFound from './pages/NotFound'
 import WaldenMinimap from './components/WaldenMiniMap.jsx'
+import MapPage from './pages/MapPage.jsx'
 
 export default function App() {
   const { pathname } = useLocation()
@@ -15,6 +16,7 @@ export default function App() {
         <Route path="/" element={<Landing />} />
         <Route path="/timeline" element={<Timeline />} />
         <Route path="/day/:id" element={<DayView />} />
+        <Route path="/map" element={<MapPage />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       {showMap && <WaldenMinimap />}
